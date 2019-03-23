7213d8d0a67c58b93d3a3ff3ab4bb154eb484311
Brett Stainken <brettstainken.student@careerdevs.com>
2019-03-23 17:29:40 +0000

    GDB

---
+++ models/employee.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+// create user Schema & model
+const EmployeeSchema = new Schema({
+   name: {
+       type: String,
+   },
+   clockin: {
+       type: Number,
+       required: [true, "Error"]
+   },
+   clockout: {
+       type: Number
+   }});
+
+
+const Employee = mongoose.model("employee", EmployeeSchema);
